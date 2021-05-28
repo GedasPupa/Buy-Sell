e388e20ef9b9a5157004e53592702f3a30e94fd2
GedasPupa <gedosas@gmail.com>
2021-05-28 13:58:02 +0300

    fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,9 +7,35 @@ class App extends React.Component {
     this.state = { 
       pay: 1000,
       buy: 1,
-      payment: "None"
+      payment: "None",
+      results: null
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  };
+
+  componentDidMount() {
+   
+    // const url2 = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
+    const url2 = "https://api.coingate.com/v2/rates?format=json";
+ 
+    fetch(url2)
+    .then(this.handleErrors)
+    .then(jsonResponse => {
+        const results = jsonResponse.trader.buy;
+        // const results = jsonResponse.Results.map(result => ({
+        //   id: 0,  
+        //   Mfr_ID: result.Mfr_ID,
+        //   Mfr_Name: result.Mfr_Name            
+        // }));
+      this.setState({ results: results });
+    }).catch(error => console.log(error.message));
   }
 
   handleInputChange(event) {
--- src/Components/BuySell/BuySell.js
+++ src/Components/BuySell/BuySell.js
@@ -4,22 +4,22 @@ import './BuySell.css';
 class BuySell extends React.Component {
     render() {
         return (
-            <form class="form" onSubmit={this.props.onSubmit}>
-                <div class="form-row">
-                    <input type="text" name="pay" placeholder="Pay" onChange={this.props.handleChange} />
+            <form className="form" onSubmit={this.props.onSubmit}>
+                <div className="form-row">
+                    <input type="number" name="pay" placeholder="Pay" onChange={this.props.handleChange} />
                 </div>
-                <div class="form-row">
-                    <input type="text" name="buy" placeholder="Buy" onChange={this.props.handleChange} />
+                <div className="form-row">
+                    <input type="number" name="buy" placeholder="Buy" onChange={this.props.handleChange} />
                 </div>
                 <p>Payment method</p> 
-                <div class="form-row">
+                <div className="form-row">
                     <select name="payment" id="payment" onChange={this.props.handleChange}>
                         <option value="bank">Bank transfer</option>
                         <option value="card">Credit/Debit card</option>
                         <option value="paypal">Paypal</option>
                     </select>
                 </div>
-                <div class="form-row">
+                <div className="form-row">
                     <button name="button" type="submit">Buy BTC</button>
                 </div>        
             </form>            
